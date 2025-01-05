74ecbd19256ca4de2d2ea94485702dbdf20943a6
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-01-05 13:23:33 -0500

    Simple script to install the Native Messaging Host manifest for Scanner

---
+++ install-manifest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+EXE_PATH=`realpath ./target/debug/resistance-advisor`
+MANIFEST_NAME=com.github.tacticallaptopbag.resistance.json
+
+cp ./native-messaging-manifest.template.json ./native-messaging-manifest.firefox.json
+sed -i -e "s%\$EXE_PATH%$EXE_PATH%" ./native-messaging-manifest.firefox.json
+sed -i -e "s%\$ALLOWED_EXT%allowed_extensions%" ./native-messaging-manifest.firefox.json
+sed -i -e "s%\$EXT_ID%resistance-advisor@extension.js%" ./native-messaging-manifest.firefox.json
+
+cp ./native-messaging-manifest.template.json ./native-messaging-manifest.chrome.json
+sed -i -e "s%\$EXE_PATH%$EXE_PATH%" ./native-messaging-manifest.chrome.json
+sed -i -e "s%\$ALLOWED_EXT%allowed_origins%" ./native-messaging-manifest.chrome.json
+sed -i -e "s%\$EXT_ID%chrome-extension://*/%" ./native-messaging-manifest.chrome.json
+
+mkdir -p /usr/lib/librewolf/native-messaging-hosts
+
+cp ./native-messaging-manifest.firefox.json /usr/lib/mozilla/native-messaging-hosts/$MANIFEST_NAME
+cp ./native-messaging-manifest.firefox.json /usr/lib/librewolf/native-messaging-hosts/$MANIFEST_NAME
+sudo cp ./native-messaging-manifest.chrome.json /etc/opt/chrome/native-messaging-hosts/$MANIFEST_NAME
+cp ./native-messaging-manifest.chrome.json ~/.var/app/io.github.ungoogled_software.ungoogled_chromium/config/chromium/NativeMessagingHosts/$MANIFEST_NAME
+
+rm ./native-messaging-manifest.firefox.json ./native-messaging-manifest.chrome.json
+
