8d64691aad26cc1f54e70233770aafd87406104d
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-01-06 08:27:51 -0500

    Need sudo to copy stuff to system files

--- install-manifest.sh
+++ install-manifest.sh
@@ -7,17 +7,16 @@ MANIFEST_NAME=com.github.tacticallaptopbag.resistance.json
 cp ./native-messaging-manifest.template.json ./native-messaging-manifest.firefox.json
 sed -i -e "s%\$EXE_PATH%$EXE_PATH%" ./native-messaging-manifest.firefox.json
 sed -i -e "s%\$ALLOWED_EXT%allowed_extensions%" ./native-messaging-manifest.firefox.json
-sed -i -e "s%\$EXT_ID%resistance-advisor@extension.js%" ./native-messaging-manifest.firefox.json
+sed -i -e "s%\$EXT_ID%resistance-scanner@extension.js%" ./native-messaging-manifest.firefox.json
 
 cp ./native-messaging-manifest.template.json ./native-messaging-manifest.chrome.json
 sed -i -e "s%\$EXE_PATH%$EXE_PATH%" ./native-messaging-manifest.chrome.json
 sed -i -e "s%\$ALLOWED_EXT%allowed_origins%" ./native-messaging-manifest.chrome.json
 sed -i -e "s%\$EXT_ID%chrome-extension://*/%" ./native-messaging-manifest.chrome.json
 
-mkdir -p /usr/lib/librewolf/native-messaging-hosts
-
-cp ./native-messaging-manifest.firefox.json /usr/lib/mozilla/native-messaging-hosts/$MANIFEST_NAME
-cp ./native-messaging-manifest.firefox.json /usr/lib/librewolf/native-messaging-hosts/$MANIFEST_NAME
+sudo mkdir -p /usr/lib/librewolf/native-messaging-hosts /usr/lib/mozilla/native-messaging-hosts /etc/opt/chrome/native-messaging-hosts
+sudo cp ./native-messaging-manifest.firefox.json /usr/lib/mozilla/native-messaging-hosts/$MANIFEST_NAME
+sudo cp ./native-messaging-manifest.firefox.json /usr/lib/librewolf/native-messaging-hosts/$MANIFEST_NAME
 sudo cp ./native-messaging-manifest.chrome.json /etc/opt/chrome/native-messaging-hosts/$MANIFEST_NAME
 cp ./native-messaging-manifest.chrome.json ~/.var/app/io.github.ungoogled_software.ungoogled_chromium/config/chromium/NativeMessagingHosts/$MANIFEST_NAME
 
